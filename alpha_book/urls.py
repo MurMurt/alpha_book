6c029a2c318f1d09bb1f91d4b0403e6e0f8afaf4
MurMurt <igataullin.g4s@mail.ru>
2017-11-20 22:34:31 +0300

    added views

--- alpha_book/urls.py
+++ alpha_book/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.books_list, name='books_list'),
-
+    url(r'^$', views.MainPageView.as_view(), name='books_list'),
+    url(r'^book/(?P<id>\d+)', views.BookPageView.as_view(), name='book')
 ]
--- alpha_book/views.py
+++ alpha_book/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from django.views import View
+
 
 def books_list(request):
     return render(request, 'alfabook/books_list.html', {})
+
+
+class MainPageView(View):
+    def get(self, request):
+        data = {
+            'top_books': [
+                {'title': 'book #1', 'id': 1},
+                {'title': 'book #2', 'id': 2},
+                {'title': 'book #3', 'id': 3},
+            ]
+        }
+        return render(request, 'alfabook/index.html', data)
+
+
+class BookPageView(View):
+    def get(self, request, id):
+        data = {
+            'book': {
+                'id': id
+            }
+        }
+        return render(request, 'alfabook/book.html', data)
